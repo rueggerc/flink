f9d7d0acfbacfd68209f6eb4f80d7d8fe0cb9670
Chris <chris.ruegger@gmail.com>
2018-02-02 07:31:14 -0500

    Streaming word count

--- MyFlink/src/main/java/com/rueggerllc/flink/SocketTextStreamWordCount.java
+++ MyFlink/src/main/java/com/rueggerllc/flink/SocketTextStreamWordCount.java
@@ -61,43 +61,36 @@ public class SocketTextStreamWordCount {
 		String hostName = args[0];
 		Integer port = Integer.parseInt(args[1]);
 
-		// set up the execution environment
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment
-				.getExecutionEnvironment();
+		// Set up the execution environment
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		// get input data
+		// Get input data
 		DataStream<String> text = env.socketTextStream(hostName, port);
 
+		// Split up the lines in pairs (2-tuples) containing: (word,1)
+		// Group by the tuple field "0" and sum up tuple field "1"
 		DataStream<Tuple2<String, Integer>> counts =
-		// split up the lines in pairs (2-tuples) containing: (word,1)
 		text.flatMap(new LineSplitter())
-		// group by the tuple field "0" and sum up tuple field "1"
 				.keyBy(0)
 				.sum(1);
 
 		counts.print();
 
-		// execute program
+		// Execute
 		env.execute("Java WordCount from SocketTextStream Example");
 	}
 
-	//
-	// 	User Functions
-	//
 
-	/**
-	 * Implements the string tokenizer that splits sentences into words as a user-defined
-	 * FlatMapFunction. The function takes a line (String) and splits it into
-	 * multiple pairs in the form of "(word,1)" (Tuple2&lt;String, Integer&gt;).
-	 */
+	// Split line into Collection of Tuple2<word, 1>
 	public static final class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
 
 		@Override
 		public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
-			// normalize and split the line
+			// Split the line into tokens
 			String[] tokens = value.toLowerCase().split("\\W+");
 
-			// emit the pairs
+			// Emit Tuple2:
+			// <word, 1>
 			for (String token : tokens) {
 				if (token.length() > 0) {
 					out.collect(new Tuple2<String, Integer>(token, 1));
