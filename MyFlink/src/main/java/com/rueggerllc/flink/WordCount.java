a96a0ffef331832676a105745fa5cfb733c7fb73
Chris Ruegger <chris.ruegger@gmail.com>
2018-02-03 15:02:18 -0500

    Flink updates

--- MyFlink/src/main/java/com/rueggerllc/flink/WordCount.java
+++ MyFlink/src/main/java/com/rueggerllc/flink/WordCount.java
@@ -43,22 +43,25 @@ public class WordCount {
 	//
 
 	public static void main(String[] args) throws Exception {
+		
+		System.out.println("HERE WE GO WITH WORD COUNT");
 
 		// set up the execution environment
 		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-		// get input data
-		DataSet<String> text = env.fromElements(
-				"To be, or not to be,--that is the question:--",
-				"Whether 'tis nobler in the mind to suffer",
-				"The slings and arrows of outrageous fortune",
-				"Or to take arms against a sea of troubles,"
-				);
+		// Get input data
+//		DataSet<String> text = env.fromElements(
+//				"To be, or not to be,--that is the question:--",
+//				"Whether 'tis nobler in the mind to suffer",
+//				"The slings and arrows of outrageous fortune",
+//				"Or to take arms against a sea of troubles",
+//				"Yo Ruge here is some extra input Foo Bar Foo Foo"
+//				);
 
+		// HDFS
+		DataSet<String> text = env.readTextFile("hdfs://captain:9000/inputs/word_count.text");
 		DataSet<Tuple2<String, Integer>> counts =
-				// split up the lines in pairs (2-tuples) containing: (word,1)
 				text.flatMap(new LineSplitter())
-				// group by the tuple field "0" and sum up tuple field "1"
 				.groupBy(0)
 				.sum(1);
 
@@ -67,23 +70,11 @@ public class WordCount {
 
 	}
 
-	//
-	// 	User Functions
-	//
 
-	/**
-	 * Implements the string tokenizer that splits sentences into words as a user-defined
-	 * FlatMapFunction. The function takes a line (String) and splits it into
-	 * multiple pairs in the form of "(word,1)" (Tuple2&lt;String, Integer&gt;).
-	 */
 	public static final class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
-
 		@Override
 		public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
-			// normalize and split the line
 			String[] tokens = value.toLowerCase().split("\\W+");
-
-			// emit the pairs
 			for (String token : tokens) {
 				if (token.length() > 0) {
 					out.collect(new Tuple2<String, Integer>(token, 1));
