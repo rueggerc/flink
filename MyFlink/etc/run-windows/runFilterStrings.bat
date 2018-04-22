128df0a8cc942b72b829ce6529d8cdc444c3525d
Chris <chris.ruegger@gmail.com>
2018-04-21 21:49:35 -0400

    Development

---
+++ MyFlink/etc/run-windows/runFilterStrings.bat
@@ -0,0 +1,2 @@
+@echo off
+flink run -c com.rueggerllc.flink.stream.FilterStrings ..\..\build\libs\MyFlink-fat-1.0.jar
---
+++ MyFlink/src/main/java/com/rueggerllc/flink/stream/FilterStrings.java
@@ -0,0 +1,50 @@
+package com.rueggerllc.flink.stream;
+
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+
+/*
+ * Start nc -l 9999
+ * Enter numbers and strings
+ */
+public class FilterStrings {
+
+	public static void main(String[] args) throws Exception {
+		
+		try {
+			StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+			DataStream<String> dataStream = env
+					.socketTextStream("localhost", 9999)
+					// .filter(new Filter());
+					.filter(s -> {
+						            try {
+						            	Double.parseDouble(s);
+						            	return true;
+						            } catch(Exception e) {
+						            	return false;
+						            }
+								 });
+			dataStream.print();
+			
+			// Start
+			env.execute("Filter Strings");
+			
+		} catch (Exception e) {
+			System.out.println("ERROR:\n" + e);
+		}
+	}
+	
+	public static class Filter implements FilterFunction<String> {
+		public boolean filter(String input) throws Exception {
+			try {
+				Double.parseDouble(input.trim());
+				return true;
+			} catch (Exception e) {
+				return false;
+			}
+		}
+	}
+}
