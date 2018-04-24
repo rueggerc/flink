0d4b0fa3c4cad52b4a3cdd93864e6f5672a9d8e4
Chris <chris.ruegger@gmail.com>
2018-04-24 07:26:04 -0400

    Dev

--- MyFlink/src/main/java/com/rueggerllc/flink/stream/FilterStrings.java
+++ MyFlink/src/main/java/com/rueggerllc/flink/stream/FilterStrings.java
@@ -17,26 +17,41 @@ public class FilterStrings {
 		
 		try {
 			StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-			DataStream<String> dataStream = env
-					.socketTextStream("localhost", 9999)
-					// .filter(new Filter());
-					.filter(s -> {
-						            try {
-						            	Double.parseDouble(s);
-						            	return true;
-						            } catch(Exception e) {
-						            	return false;
-						            }
-								 });
-			dataStream.print();
-			
-			// Start
-			env.execute("Filter Strings");
-			
+			execute0(env);
 		} catch (Exception e) {
 			System.out.println("ERROR:\n" + e);
 		}
 	}
+	
+	private static void execute0(StreamExecutionEnvironment env) throws Exception {
+		DataStream<Long> dataStream = env
+				.socketTextStream("localhost", 9999)
+				.filter(new Filter())
+				.map(new Rounder());
+		dataStream.print();
+		
+		// Start
+		env.execute("FilterAndRound");
+	}
+	
+	private static void execute1(StreamExecutionEnvironment env) throws Exception {
+		DataStream<String> dataStream = env
+				.socketTextStream("localhost", 9999)
+				.filter(s -> {
+					            try {
+					            	Double.parseDouble(s);
+					            	return true;
+					            } catch(Exception e) {
+					            	System.out.println("NOPE=" + s);
+					            	return false;
+					            }
+							 });
+		dataStream.print();
+		
+		// Start
+		env.execute("Filter Strings");		
+	}
+	
 	
 	public static class Filter implements FilterFunction<String> {
 		public boolean filter(String input) throws Exception {
@@ -44,18 +59,16 @@ public class FilterStrings {
 				Double.parseDouble(input.trim());
 				return true;
 			} catch (Exception e) {
+				System.out.println("Filter Out=" + input);
 				return false;
 			}
 		}
 	}
 	
-	public static class Mapper implements MapFunction<String,String> {
-		public String map(String input) throws Exception {
-			try {
-				return null;
-			} catch (Exception e) {
-				return null;
-			}
+	public static class Rounder implements MapFunction<String, Long> {
+		public Long map(String input) throws Exception {
+			double d = Double.parseDouble(input);
+			return Math.round(d);
 		}
 	}
 	
